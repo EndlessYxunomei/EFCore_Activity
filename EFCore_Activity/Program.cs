76000ef06ea0433db89cce8af35932d247cff036
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2023-10-08 09:37:46 +0300

    добавлены стартовые файлы

--- EFCore_Activity/Program.cs
+++ EFCore_Activity/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace EFCore_Activity
+﻿using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Activity
 {
     public class Program
     {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BuildOptions();
+        }
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
         }
     }
 }
---
+++ EFCore_DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        //пустой конструктор для возможности scuffold базы данных
+        public InventoryDbContext() { }
+        //конструктор для dependensy injection
+        public InventoryDbContext(DbContextOptions options) : base(options) { }
+    }
+}
