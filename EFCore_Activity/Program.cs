2ea2bb2597a4c928a950e240f05ce8cdb2db5fdc
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2023-10-08 09:16:56 +0300

    Add project files.

---
+++ EFCore_Activity/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore_Activity
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
