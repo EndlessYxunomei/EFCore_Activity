a1db5dd2b1e7661c3a256ff1984f35c0a214ccad
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2023-10-08 10:30:35 +0300

    глава 3 закончена

--- EFCore_Activity/Program.cs
+++ EFCore_Activity/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_DBLibrary;
+using InventoryModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -12,12 +13,45 @@ namespace EFCore_Activity
         static void Main(string[] args)
         {
             BuildOptions();
+            EnsureItems();
+            ListInventory();
         }
         static void BuildOptions()
         {
             _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
             _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
             _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+        }
+        static void EnsureItems()
+        {
+            EnsureItem("Batman Begins");
+            EnsureItem("Inception");
+            EnsureItem("Remember te Titans");
+            EnsureItem("Star Wars: The Empire Strikes Back");
+            EnsureItem("Top Gun");
+        }
+        private static void EnsureItem(string name)
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                //опрееляем есть ли подобные записи
+                var existingItem = db.Items.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+                if (existingItem == null)
+                {
+                    //если нет, то создаем новый объект и добавлем его
+                    var item = new Item() { Name = name };
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+        private static void ListInventory()
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                var items = db.Items.OrderBy(x => x.Name).ToList();
+                items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+            }
         }
     }
 }
