72be54c27033fbd9c6a537fcbdb8485494f3e173
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2023-11-04 09:22:41 +0300

    12-2 пройден

---
+++ InventoryManagerIntegrationTests/InventoryMapper.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using InventoryModels.DTOs;
+using InventoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagerIntegrationTests
+{
+    public class InventoryMapper : Profile
+    {
+        public InventoryMapper()
+        {
+            CreateMaps();
+        }
+        private void CreateMaps()
+        {
+            CreateMap<Item, ItemDTO>().ReverseMap();
+            CreateMap<CategoryDetail, CategoryDetailDTO>()
+                .ForMember(x => x.Color, opt => opt.MapFrom(y => y.ColorName))
+                .ForMember(x => x.Value, opt => opt.MapFrom(y => y.ColorValue))
+                .ReverseMap()
+                .ForMember(y => y.ColorValue, opt => opt.MapFrom(x => x.Value))
+                .ForMember(y => y.ColorName, opt => opt.MapFrom(x => x.Color));
+            CreateMap<Category, CategoryDTO>()
+                .ForMember(x => x.Category, opt => opt.MapFrom(y => y.Name))
+                .ForMember(x => x.CategotyDetail, opt => opt.MapFrom(y => y.CategoryDetail))
+                .ReverseMap()
+                .ForMember(y => y.Name, opt => opt.MapFrom(x => x.Category));
+            CreateMap<Item, CreateOrUpdateItemDTO>()
+                .ReverseMap()
+                .ForMember(x => x.Category, opt => opt.Ignore());
+        }
+    }
+}
